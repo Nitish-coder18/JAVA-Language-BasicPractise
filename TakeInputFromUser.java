67c1b49121af7996f0ff7eae18b704287edd7a5c
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-06-01 08:14:44 +0530

    Taking Input From User

---
+++ TakeInputFromUser.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class TakeInputFromUser {
+    public static void main(String args[]) {
+        Scanner S = new Scanner(System.in);
+        System.out.print("Enter First Number - ");
+        int a = S.nextInt();
+        System.out.print("Enter Second Number - ");
+        int b = S.nextInt();
+    }
+}
